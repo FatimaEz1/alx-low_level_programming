a5c9fc69c62b571c0524d1747f225c47f786a33f
FatimaEz1 <frajy67@gmail.com>
2023-03-21 16:48:29 -0700

    all tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,13 @@
  * Return: Always 0 (Success)
 */
 
-void print_alphabet(void) {
-    char c;
-    for (c = 'a'; c <= 'z'; c++) {
-        _putchar(c);
+void print_alphabet(void) 
+    {
+    	char c;
+
+    	for (c = 'a'; c <= 'z'; c++) {
+        	_putchar(c);
     }
+
     _putchar('\n');
 }
