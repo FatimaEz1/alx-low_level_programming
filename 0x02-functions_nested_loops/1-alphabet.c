abf5c5ac0fd94700501782cb2f422cc867d90267
FatimaEz1 <frajy67@gmail.com>
2023-03-21 16:53:56 -0700

    all tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,16 @@
  * Description: prints the alphabet, in lowercase,
  *
  * print_alphabet is a function that prints the lowercase alphabet followed by a newline character using _putchar twice.
+*
 */
 
-void print_alphabet(void) 
-    {
-    	char c;
+void print_alphabet(void)
+{
+    char c;
 
-    	for (c = 'a'; c <= 'z'; c++) {
-        	_putchar(c);
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
     }
 
     _putchar('\n');
