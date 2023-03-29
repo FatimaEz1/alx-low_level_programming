85641faa9528e51bd46d957e65d3ea6e406e4c91
FatimaEz1 <frajy67@gmail.com>
2023-03-28 19:13:50 -0700

    all tasks

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int rand = 0, c = 0;
+	int rd = 0, c = 0;
 	time_t t;
 
 	srand((unsigned int) time(&t));
 	while (c < 2772)
 	{
-		rand = rand() % 128;
-		if ((c + rand) > 2772)
+		rd = rand() % 128;
+		if ((c + rd) > 2772)
 			break;
-		c = c + rand;
-		printf("%c", rand);
+		c = c + rd;
+		printf("%c", rd);
 	}
 	printf("%c\n", (2772 - c));
 	return (0);
