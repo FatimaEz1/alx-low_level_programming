cf71cb7bd1001fe69aa3a18c5e27002dd12da34a
FatimaEz1 <frajy67@gmail.com>
2023-03-28 18:12:42 -0700

    all tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "studio.h"
+/**
+ * print_rev - print a string in reverse, followed by a new line.
+ * @s: string.
+ * Return: no return.
+ */
+void print_rev(char *s)
+{
+int len = 0;
+while (s[len] != '\0')
+{  // get length of string
+len++;
+}
+for (int i = len - 1; i >= 0; i--)
+{  // print string in reverse order
+printf("%c", s[i]);
+}
+printf("\n");  // print new line
+}
