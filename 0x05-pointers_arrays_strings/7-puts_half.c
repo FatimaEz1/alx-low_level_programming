3bf1469ac2f088d991d696e006f31961cf0c623b
FatimaEz1 <frajy67@gmail.com>
2023-03-28 18:49:25 -0700

    all tasks

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,26 +5,17 @@
  * @str: char array string type
  * Description: If the number of characters is odd, the function should print (length - 1) / 2 of the string
  */
-
 void puts_half(char *str)
 {
-int len = strlen(str);
-int n = (len - 1) / 2;
-if (len % 2 == 0)
-{
-int i = n;
-for (; i < len; i++)
-{
-putchar(str[i]);
-}
-}
-else
-{
-int i = n + 1;
-for (; i < len; i++)
-{
-putchar(str[i]);
-}
-}
-putchar('\n');
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
