6c0c7f17e0f5c3f501c8a4f630800cec94279be8
FatimaEz1 <frajy67@gmail.com>
2023-03-28 18:06:31 -0700

    all tasks

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function that prints a string, followed by a new line,
+ * @str: pointer that prints the string
+ * Return: void
+ */
+void _puts(char *str)
+{
+int i = 0;
+while(str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}
